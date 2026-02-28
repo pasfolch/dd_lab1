8ab6b5ce4760f94e6e0c2ed92f3709a2303f2152
Jordi <pasfolch@alumni.uv.es>
2026-02-28 13:15:43 +0100

    fsm structure

--- spi_slave/rtl/spi_slave.sv
+++ spi_slave/rtl/spi_slave.sv
@@ -13,34 +13,127 @@ module spi_slave
     output  wire [NB_ADDR - 1 : 0]  addr_out, // Address output for memory access
     output  wire [NB_DATA - 1 : 0]  wr_data , // Data to be written to memory.
     output  wire                    wr_req  , // Write request signal. Must rise on the last SCLK pulse of the data word on SPI write frame to trigger the memory write operation
-    output  wire                    rd_req  , // Read request signal. Must rise on the last SCLK pulse of the address word on the SPI read frame to trigger the memory read operation
+    output  wire                    rd_req    // Read request signal. Must rise on the last SCLK pulse of the address word on the SPI read frame to trigger the memory read operation
 );
 
 
     // byte counter
 
     localparam NB_COUNT = $clog2(NB_DATA);
-    reg [NB_COUNT - 1 : 0] count;
-    always @() begin
-        
+
+    reg  [NB_COUNT - 1 : 0] count;
+    wire [NB_COUNT - 1 : 0] count_next;
+    wire                    count_limit;
+
+    assign count_next  = count + 1'b1;   
+    assign count_limit = (count_next >= NB_DATA);
+
+    always @(posedge sclk or negedge resetb) begin
+        if ((!resetb) | count_limit) begin
+            count <= {NB_DATA{1'b0}};
+        end else begin
+            count <= count_next;
+        end
     end
 
-    reg [NB_DATA - 1 : 0] sr;
+    // shift register to store bytes
 
+    reg  [NB_DATA - 1 : 0] sr;
+
+    always @(posedge sclk or negedge resetb) begin
+        if (!resetb) begin
+            sr <= {NB_DATA{1'b0}};
+        end else begin
+            sr <= {sr[NB_DATA - 2 : 1], mosi};
+        end
+    end
 
     // fsm
 
-    typedef enum logic [1:0] {
-        STATE_IDLE,
-        STATE_HEAD,
-        STATE_ADDR,
-        STATE_EXEC
-    } state_t;
+    typedef enum logic [2:0] {
+        STATE_IDLE  = 3'b000,
+        STATE_HEAD  = 3'b001,
+        STATE_ADDR  = 3'b010,
+        STATE_READ  = 3'b011,
+        STATE_WRITE = 3'b100
+    } state_e;
 
-    state_t current_state, next_state;
+    state_e state, next;
 
-    always @() begin
-        
+    always @(posedge sclk or negedge resetb) begin
+        if ((!resetb) | csb) begin
+            state <= STATE_IDLE;
+        end else begin
+            state <= next;
+        end
+    end
+
+    always @(*) begin
+        case (state)
+            STATE_IDLE: begin
+                next = (csb)? STATE_IDLE : STATE_HEAD;
+            end 
+            STATE_HEAD: begin
+                next = (count_limit)? STATE_ADDR : STATE_HEAD;
+            end
+            STATE_ADDR: begin
+                next = (count_limit & (sr == {NB_DATA{1'b0}}))? STATE_WRITE : STATE_READ;
+            end
+            STATE_READ: begin
+                next = (count_limit)? STATE_IDLE : STATE_READ;
+            end
+            STATE_WRITE: begin
+                next = (count_limit)? STATE_IDLE : STATE_WRITE;
+            end
+            default: next = STATE_IDLE;
+        endcase
+    end
+
+    always @(*) begin
+        case (state)
+            STATE_IDLE: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+            STATE_HEAD: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+            STATE_ADDR: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+            STATE_READ: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+            STATE_WRITE: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+            default: begin
+                miso     =          1'b0;
+                addr_out = {NB_ADDR{1'b0}};
+                wr_data  = {NB_DATA{1'b0}};
+                wr_req   =          1'b0;
+                rd_req   =          1'b0;
+            end
+        endcase
     end
 
 
